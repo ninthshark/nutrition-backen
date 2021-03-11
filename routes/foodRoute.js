c6f8a96810140a82541fb016321a5352d9d691b9
Kiattisak Dujda <kiattisak.dujda@gmail.com>
2021-03-11 16:15:23 +0000

    refactored GET route

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -3,26 +3,38 @@ const Nutri = require("../models/nutri.model");
 
 foodRoute.get("/search?", async (req, res) => {
   const searchTerm = req.query.name;
-  try {
-    const result = await Nutri.find(
-      { $text: { $search: searchTerm } },
-      { score: { $meta: "textScore" } }
-    ).sort({ score: { $meta: "textScore" } });
-
-    res.json(result).end();
-  } catch (err) {
-    res.status(400).send(err);
-  }
-});
-
-foodRoute.get("/search?", async (req, res) => {
   const searchId = req.query.id;
-  try {
-    const result = await Nutri.findById({ _id: searchId });
-    res.json(result).end();
-  } catch (err) {
-    res.status(400).send(err);
+
+  if (searchTerm) {
+    try {
+      const result = await Nutri.find(
+        { $text: { $search: searchTerm } },
+        { score: { $meta: "textScore" } }
+      ).sort({ score: { $meta: "textScore" } });
+
+      res.json(result).end();
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  }
+  if (searchId) {
+    try {
+      const result = await Nutri.findById({ _id: searchId });
+      res.json(result).end();
+    } catch (err) {
+      res.status(400).send(err);
+    }
   }
 });
+
+// foodRoute.get("/search?", async (req, res) => {
+//   const searchId = req.query.id;
+//   try {
+//     const result = await Nutri.findById({ _id: searchId });
+//     res.json(result).end();
+//   } catch (err) {
+//     res.status(400).send(err);
+//   }
+// });
 
 module.exports = foodRoute;
